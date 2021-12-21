0a77fb6737bf23ad4ce7db16e59c80f8c36b7f96
Mohammed-Alsenani2 <Mohammed.Mosa.eam.js@tuwaiq.edu.sa>
2021-12-21 08:04:48 +0300

    EditStores

--- inventory-mangement-application/pages/index.js
+++ inventory-mangement-application/pages/index.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { Button, Modal, Form, Link } from "react-bootstrap";
+import { Button, Modal, Form } from "react-bootstrap";
+import Link from "next/link";
 import Layout from "../components/Layout";
 import axios from "axios";
 import styles from "../styles/Home.module.css";
@@ -29,17 +30,6 @@ export default function Home() {
   const [name, setName] = useState("");
   const [managerName, setManagerName] = useState("");
 
-  // Start User Data
-  const [users, setUser] = useState([]);
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
-  const [role, setRole] = useState("");
-  const [isUserChanged, setIsUserChanged] = useState(false);
-  // End User Data
-
   // Start Role Data
   const [addRole, setAddRole] = useState("");
   const [roles, setRoles] = useState([]);
@@ -64,30 +54,6 @@ export default function Home() {
     const roles = await resRole.json();
     setRoles(roles);
   }, [isRoleChanged]);
-
-  function saveUser() {
-    axios
-      .post("http://localhost:5000/users/save", {
-        name: userName,
-        email: email,
-        password: password,
-        phone: phone,
-        address: address,
-        role: role,
-      })
-      .then((data) => {
-        if (data) {
-          setIsUserChanged(true);
-        }
-      })
-      .catch((error) => console.log(error));
-  }
-
-  useEffect(async () => {
-    const resUsers = await fetch("http://localhost:5000/users/show");
-    const users = await resUsers.json();
-    setUser(users);
-  }, [isUserChanged]);
 
   useEffect(async () => {
     const resStores = await fetch("http://localhost:5000/stores/show");
@@ -114,19 +80,18 @@ export default function Home() {
       <div className={styles.container}>
         <main className={styles.main}>
           <div className={styles.grid}>
-            <Button
-              variant="primary"
-              className={styles.btn}
-            >
-              Store
-            </Button>
-            <Button
-              variant="primary"
-              className={styles.btn}
-              onClick={handleShowUser}
-            >
-              User
-            </Button>
+            <Link href="/store">
+              <Button variant="primary" className={styles.btn}>
+                Store
+              </Button>
+            </Link>
+
+            <Link href="/user">
+              <Button variant="primary" className={styles.btn}>
+                User
+              </Button>
+            </Link>
+
             <Button
               variant="primary"
               className={styles.btn}
@@ -135,168 +100,8 @@ export default function Home() {
               Role
             </Button>
           </div>
-          {/* <div className={styles.grid}>
-            {stores &&
-              stores.map((store) => {
-                return (
-                  <a href="" className={styles.card}>
-                    <h2>{store.name}</h2>
-                    <h3>{store.managerName}</h3>
-                  </a>
-                );
-              })}
-          </div> */}
         </main>
       </div>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Add Store</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form className={styles.form}>
-            <Form.Group className="mb-3">
-              <Form.Label>
-                Store Name<span>*</span>
-              </Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter store name"
-                autoComplete="off"
-                onChange={(e) => setName(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-              <Form.Label>
-                Manager Name<span>*</span>
-              </Form.Label>
-              <Form.Select onChange={(e) => setManagerName(e.target.value)}>
-                <option value="default">Choose a Manager</option>
-                {users &&
-                  users.map((user) => {
-                    return <option value={user.name}>{user.name}</option>;
-                  })}
-              </Form.Select>
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            variant="secondary"
-            onClick={handleClose}
-            className={styles.btnClose}
-          >
-            Close
-          </Button>
-          <Button
-            variant="primary"
-            className={styles.btnAdd}
-            onClick={() => {
-              addStore();
-              handleClose();
-            }}
-          >
-            Add Store
-          </Button>
-        </Modal.Footer>
-      </Modal>
-
-      <Modal show={showUser} onHide={handleCloseUser}>
-        <Modal.Header closeButton>
-          <Modal.Title>Add User</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form className={styles.form}>
-            <Form.Group className="mb-3">
-              <Form.Label>
-                Name<span>*</span>
-              </Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter name"
-                autoComplete="off"
-                onChange={(e) => setUserName(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-              <Form.Label>
-                Email address<span>*</span>
-              </Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                autoComplete="off"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>
-                Password<span>*</span>
-              </Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-              <Form.Label>Phone</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter phone"
-                autoComplete="off"
-                onChange={(e) => setPhone(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-              <Form.Label>Address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter address"
-                autoComplete="off"
-                onChange={(e) => setAddress(e.target.value)}
-              />
-            </Form.Group>
-
-            <Form.Group className="mb-3">
-              <Form.Label>Role</Form.Label>
-              <Form.Select onChange={(e) => setRole(e.target.value)}>
-                <option value="default">Choose a Role</option>
-                {roles &&
-                  roles.map((role) => {
-                    return <option value={role.name}>{role.name}</option>;
-                  })}
-              </Form.Select>
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            variant="secondary"
-            className={styles.btnClose}
-            onClick={() => {
-              handleCloseUser();
-            }}
-          >
-            Close
-          </Button>
-          <Button
-            variant="primary"
-            className={styles.btnAdd}
-            onClick={() => {
-              saveUser();
-              handleCloseUser();
-            }}
-          >
-            Add User
-          </Button>
-        </Modal.Footer>
-      </Modal>
 
       <Modal show={showRole} onHide={handleCloseRole}>
         <Modal.Header closeButton>
--- inventory-mangement-application/pages/store.js
+++ inventory-mangement-application/pages/store.js
@@ -3,220 +3,208 @@ import Layout from "../components/Layout";
 import styles from "../styles/store.module.css";
 import axios from "axios";
 import {
-    Button,
-    Modal,
-    Form,
-    Table,
-    Container,
-    Row,
-    Col,
+  Button,
+  Modal,
+  Form,
+  Table,
+  Container,
+  Row,
+  Col,
 } from "react-bootstrap";
 import { VscTrash } from "react-icons/vsc";
 import { FaRegEdit } from "react-icons/fa";
 
 export default function store() {
-    // Stores data
-    const [stores, setStores] = useState([]);
-    const [isStoreChanged, setIsStoreChanged] = useState(false);
+  // Stores data
+  const [stores, setStores] = useState([]);
+  const [isStoreChanged, setIsStoreChanged] = useState(false);
 
-    // Start Model Store
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    // End Model Store
+  // Start Model Store
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  // End Model Store
 
-    // Start Store Data
-    const [name, setName] = useState("");
-    const [managerName, setManagerName] = useState("");
-    // End Store Data
+  // Start Store Data
+  const [name, setName] = useState("");
+  const [managerName, setManagerName] = useState("");
+  const [updateId, setId] = useState("");
+  // End Store Data
 
-    // Start User Data
-    const [users, setUser] = useState([]);
-    const [isUserChanged, setIsUserChanged] = useState(false);
-    // End User Data
+  // Start User Data
+  const [users, setUser] = useState([]);
+  const [isUserChanged, setIsUserChanged] = useState(false);
+  // End User Data
 
-    useEffect(async () => {
-        const resUsers = await fetch("http://localhost:5000/users/show");
-        const users = await resUsers.json();
-        setUser(users);
-    }, [isUserChanged]);
+  useEffect(async () => {
+    const resUsers = await fetch("http://localhost:5000/users/show");
+    const users = await resUsers.json();
+    setUser(users);
+  }, [isUserChanged]);
 
-    useEffect(async () => {
-        const resStores = await fetch("http://localhost:5000/stores/show");
-        const stores = await resStores.json();
-        setStores(stores);
-    }, [isStoreChanged]);
+  useEffect(async () => {
+    const resStores = await fetch("http://localhost:5000/stores/show");
+    const stores = await resStores.json();
+    setStores(stores);
+  }, [isStoreChanged]);
 
-    function addStore() {
-        axios
-            .post("http://localhost:5000/stores/save", {
-                name: name,
-                managerName: managerName,
-            })
-            .then((data) => {
-                if (data) {
-                    setIsStoreChanged(true);
-                }
-            })
-            .catch((error) => console.log(error));
+  function addStore() {
+    if (updateId) {
+      axios
+        .put("http://localhost:5000/stores/update/" + updateId, {
+          name: name,
+          managerName: managerName,
+        })
+        .then((data) => {
+          if (data) {
+            setIsStoreChanged(true);
+            setName("");
+            setManagerName("default");
+            setId("");
+          }
+        })
+        .catch((error) => console.log(error));
+    } else {
+      axios
+        .post("http://localhost:5000/stores/save", {
+          name: name,
+          managerName: managerName,
+        })
+        .then((data) => {
+          if (data) {
+            setIsStoreChanged(true);
+            setName("");
+            setManagerName("default");
+            setId("");
+          }
+        })
+        .catch((error) => console.log(error));
     }
+  }
 
-    function deleteStore(id) {
-        console.log(id)
-        axios
-            .put("http://localhost:5000/stores/delete/" + id, {
-                isDeleted: true,
-            })
-            .then((data) => {
-                if (data) {
-                    setIsStoreChanged(true);
-                }
-            })
-            .catch((error) => console.log(error));
-    }
+  function updateStore(id) {
+    console.log(id);
+    fetch("http://localhost:5000/stores/show/" + id)
+      .then((response) => response.json())
+      .then((data) => {
+        setName(data[0].name);
+        setManagerName(data[0].managerName);
+        setId(data[0]._id);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    return (
-        <Layout>
-            <div className={styles.container}>
-                <main className={styles.main}>
-                    <Container>
-                        <Row>
-                            <Col>
-                                <Form.Group className="mb-3">
-                                    <Form.Label>
-                                        Store Name<span>*</span>
-                                    </Form.Label>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Enter store name"
-                                        autoComplete="off"
-                                        onChange={(e) => setName(e.target.value)}
-                                    />
-                                </Form.Group>
-                            </Col>
-                            <Col>
-                                <Form.Group className="mb-3">
-                                    <Form.Label>
-                                        Manager Name<span>*</span>
-                                    </Form.Label>
-                                    <Form.Select onChange={(e) => setManagerName(e.target.value)}>
-                                        <option value="default">Choose a Manager</option>
-                                        {users &&
-                                            users.map((user) => {
-                                                return <option value={user.name}>{user.name}</option>;
-                                            })}
-                                    </Form.Select>
-                                </Form.Group>
-                            </Col>
-                            <Col>
-                                <Button
-                                    variant="primary"
-                                    className={styles.btnAdd}
-                                    onClick={() => {
-                                        addStore();
-                                        handleClose();
-                                    }}
-                                >
-                                    Add Store
-                                </Button>
-                            </Col>
-                        </Row>
-                        <br></br>
-                        <hr></hr>
-                        <br></br>
-                        <Row>
-                            <Col>
-                                <Table striped bordered hover>
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>Store Name</th>
-                                            <th>Manager Name</th>
-                                            <th>Edit</th>
-                                            <th>Delete</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {stores &&
-                                            stores.map((store) => {
-                                                return (
-                                                    <tr>
-                                                        <td>1</td>
-                                                        <td>{store.name}</td>
-                                                        <td>{store.managerName}</td>
-                                                        <td><Button><FaRegEdit className={styles.FaRegEdit} /></Button></td>
-                                                        <td><Button><VscTrash className={styles.VscTrash}
-                                                            onClick={() => {
-                                                                deleteStore(store._id);
-                                                            }}
-                                                        /></Button>
-                                                        </td>
-                                                    </tr>
-                                                );
-                                            })}
+  function deleteStore(id) {
+    console.log(id);
+    axios
+      .put("http://localhost:5000/stores/delete/" + id, {
+        isDeleted: true,
+      })
+      .then((data) => {
+        if (data) {
+          setIsStoreChanged(true);
+        }
+      })
+      .catch((error) => console.log(error));
+  }
 
-                                    </tbody>
-                                </Table>
-                            </Col>
-                        </Row>
-                    </Container>
-
-                    <div className={styles.grid}>
-                        {/* <Button
-              variant="primary"
-              className={styles.btn}
-              onClick={handleShow}
-            >
-              Store
-            </Button> */}
-                    </div>
-                </main>
-            </div>
-            {/* <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Add Store</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form className={styles.form}>
-                        <Form.Group className="mb-3">
-                            <Form.Label>
-                                Store Name<span>*</span>
-                            </Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="Enter store name"
-                                autoComplete="off"
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>
-                                Manager Name<span>*</span>
-                            </Form.Label>
-                            <Form.Select onChange={(e) => setManagerName(e.target.value)}>
-                                <option value="default">Choose a Manager</option>
-                                {users &&
-                                    users.map((user) => {
-                                        return <option value={user.name}>{user.name}</option>;
-                                    })}
-                            </Form.Select>
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-
-                    <Button
-                        variant="primary"
-                        className={styles.btnAdd}
-                        onClick={() => {
-                            addStore();
-                            handleClose();
-                        }}
-                    >
-                        Add Store
-                    </Button>
-                </Modal.Footer>
-            </Modal> */}
-        </Layout>
-    );
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Container>
+            <Row>
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>
+                    Store Name<span>*</span>
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={name}
+                    placeholder="Enter store name"
+                    autoComplete="off"
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="mb-3">
+                  <Form.Label>
+                    Manager Name<span>*</span>
+                  </Form.Label>
+                  <Form.Select onChange={(e) => setManagerName(e.target.value)}>
+                    <option value="default">Choose a Manager</option>
+                    {users &&
+                      users.map((user) => {
+                        return <option value={user.name}>{user.name}</option>;
+                      })}
+                  </Form.Select>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Button
+                  variant="primary"
+                  className={styles.btnAdd}
+                  onClick={() => {
+                    addStore();
+                  }}
+                >
+                  Add Store
+                </Button>
+              </Col>
+            </Row>
+            <hr></hr>
+            <Row>
+              <Col>
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>Store Name</th>
+                      <th>Manager Name</th>
+                      <th>Edit</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {stores &&
+                      stores.map((store) => {
+                        return (
+                          <tr>
+                            <td>{store.name}</td>
+                            <td>{store.managerName}</td>
+                            <td>
+                              <Button
+                                className={styles.btnEdit}
+                                onClick={() => {
+                                  updateStore(store._id);
+                                }}
+                              >
+                                <FaRegEdit className={styles.FaRegEdit} />
+                              </Button>
+                            </td>
+                            <td>
+                              <Button
+                                className={styles.btnEdit}
+                                onClick={() => {
+                                  deleteStore(store._id);
+                                }}
+                              >
+                                <VscTrash className={styles.VscTrash} />
+                              </Button>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                  </tbody>
+                </Table>
+              </Col>
+            </Row>
+          </Container>
+        </main>
+      </div>
+    </Layout>
+  );
 }
